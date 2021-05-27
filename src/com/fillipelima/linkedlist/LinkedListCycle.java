6066ae5221e7a7190d2336d0602724effc5a9089
Fillipe Lima <fillipe.lima@gmail.com>
2021-05-27 16:02:40 -0300

    more

---
+++ src/com/fillipelima/linkedlist/LinkedListCycle.java
@@ -0,0 +1,28 @@
+package com.fillipelima.linkedlist;
+
+class LinkedListCycle {
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null)
+			return false;
+		ListNode walker = head;
+		ListNode runner = head;
+		while (runner.next != null && runner.next.next != null) {
+			walker = walker.next;
+			runner = runner.next.next;
+			if (walker == runner)
+				return true;
+		}
+		return false;
+	}
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+}
