8814fd5a78ce0f169a363c52ca0ec621d4f6333e
Fillipe Lima <fillipe.lima>
2022-01-19 17:39:59 -0300

    Recursive merge two linked lists

--- src/com/fillipelima/linkedlist/MergeTwoSortedLits.java
+++ src/com/fillipelima/linkedlist/MergeTwoSortedLits.java
@@ -53,6 +53,22 @@ class MergeTwoSortedLits {
         }   
         return head;
     }
+    
+    // Recursive
+    public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2) {
+    	
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+    
 	class ListNode {
 		int val;
 		ListNode next;
